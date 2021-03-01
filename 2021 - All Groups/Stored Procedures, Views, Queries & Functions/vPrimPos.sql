0017d2f4834272828e1e9b55f34da4a1b9db422d
JHDatz <72771399+JHDatz@users.noreply.github.com>
2021-03-01 14:19:27 -0500

    Add files via upload

---
+++ 2021 - All Groups/Stored Procedures, Views, Queries & Functions/vPrimPos.sql
@@ -0,0 +1,18 @@
+CREATE VIEW lahman.vPrimPos AS
+	SELECT 
+		playerID,
+		yearID,
+		teamID,
+		lgID,
+		POS,
+		sum(G) as Gtot
+	FROM 
+		fielding
+	GROUP BY
+		playerID, 
+		yearID, 
+		teamID,
+		lgID,
+		POS
+	HAVING
+		IF(POS = 'OF' and yearID > 1995, 1, 0) = 0;
