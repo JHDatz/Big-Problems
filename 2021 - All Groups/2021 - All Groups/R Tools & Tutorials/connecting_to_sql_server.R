3ceb8d4fcd33c5cf4fd458cd29265f78c47568d1
JHDatz <72771399+JHDatz@users.noreply.github.com>
2021-03-01 14:16:02 -0500

    Delete 2021 - All Groups/2021 - All Groups directory

--- 2021 - All Groups/2021 - All Groups/R Tools & Tutorials/connecting_to_sql_server.R
+++
@@ -1,9 +0,0 @@
-library(RMySQL)
-require(DBI)
-
-conn <- dbConnect(MySQL(), 
-                  dbname = "lahman",
-                  user = "redacted", 
-                  password = "redacted",
-                  host = "redacted",
-                  port = 3306)
