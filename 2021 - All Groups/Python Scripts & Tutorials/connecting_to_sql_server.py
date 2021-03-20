e08ea0c71fb7968a2616ccd669776f9d675dcbe7
JHDatz <72771399+JHDatz@users.noreply.github.com>
2021-03-20 14:11:39 -0400

    Add files via upload

--- 2021 - All Groups/Python Scripts & Tutorials/connecting_to_sql_server.py
+++ 2021 - All Groups/Python Scripts & Tutorials/connecting_to_sql_server.py
@@ -1,3 +1,6 @@
+# If running in Jupyter Notebook, you'll need to make changes in Jupyter's config file by using the following link:
+# https://stackoverflow.com/questions/43288550/iopub-data-rate-exceeded-in-jupyter-notebook-when-viewing-image
+
 import mysql.connector
 
 conn = mysql.connector.connect(user = 'redacted', password = 'redacted',
