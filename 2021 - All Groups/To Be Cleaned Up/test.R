06b57c6dd01d2d7599a898e78ec3c763b1daa35d
JHDatz <72771399+JHDatz@users.noreply.github.com>
2021-03-27 13:37:39 -0400

    Add files via upload

--- 2021 - All Groups/To Be Cleaned Up/test.R
+++ 2021 - All Groups/To Be Cleaned Up/test.R
@@ -3,15 +3,56 @@ library(retro)
 library(RMySQL)
 require(DBI)
 
-db <- src_mysql("retrosheet", user = 'root', password='new_password')
-retro <- etl("retro", db = db, dir = '~/Desktop/retro')
-  
+setwd("~/Desktop")
+
+db <- src_mysql("retrosheet", user = 'root')
+retro <- etl("retro", db = db, dir = 'C:\\Users\\josep\\Desktop')
+
 retro %>%
-  etl_init() %>%
-  etl_update(season = 1980:2020)
+  etl_init()
 
-dbSendQuery(localConn, 'SET NAMES utf8mb4')
+#localConn <- dbConnect(MySQL(), dbname = 'retrosheet', user = 'root', password='new_password')
+#data <- dbGetQuery(localConn, n = -1, "select * from events")
 
-localConn <- dbConnect(MySQL(), dbname = 'retrosheet', user = 'root', password='new_password')
+retro %>% 
+  etl_extract(season = 1980:2020) %>% 
+  etl_transform(season = 1980:2020)
 
-data <- dbGetQuery(localConn, n = -1, "select * from events")
+setwd('C:\\Users\\josep\\Desktop\\load')
+
+files <- as.character(list.files(pattern = "EVN"))
+
+commandEvents <- paste0(".\\cwevent -n -f 0-96 -x 0-62 -y ", substring(files, 1, 4), ' ', files, ' > ', 
+                  substring(files, 1, 4), substring(files, 5, 7), '_events.csv')
+
+commandGames <- paste0(".\\cwgame -n -f 0-83 -y ", substring(files, 1, 4), ' ', files, ' > ', 
+                       substring(files, 1, 4), substring(files, 5, 7), '_games.csv')
+
+commandSubs <- paste0(".\\cwsub -n -y ", substring(files, 1, 4), ' ', files, ' > ', 
+                      substring(files, 1, 4), substring(files, 5, 7), '_subs.csv')
+
+commands <- c(commandEvents, commandGames, commandSubs)
+
+map(commands, shell)
+
+files <- as.character(list.files(pattern = 'events.csv|games.csv|subs.csv'))
+
+upload_retrosheet_files <- function(file, conn) {
+  df <- read.csv(file, encoding = "UTF-8")
+  
+  if (length(grep('event', file)) == 1) {
+    
+    dbWriteTable(conn, name = "events", value = df, append = TRUE, row.names = FALSE)
+    
+  } else if (length(grep('game', file)) == 1) {
+    
+    dbWriteTable(conn, name = "games", value = df, append = TRUE, row.names = FALSE)
+    
+  } else {
+    
+    dbWriteTable(conn, name = "subs", value = df, append = TRUE, row.names = FALSE)
+    
+  }
+}
+
+map(files, upload_retrosheet_files, conn=conn)
