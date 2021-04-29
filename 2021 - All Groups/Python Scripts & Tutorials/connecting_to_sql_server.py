c9ae84ef8517aafd6a29a62d50f9a1e4ffff7814
JHDatz <72771399+JHDatz@users.noreply.github.com>
2021-04-29 17:31:17 -0400

    Add files via upload

--- 2021 - All Groups/Python Scripts & Tutorials/connecting_to_sql_server.py
+++ 2021 - All Groups/Python Scripts & Tutorials/connecting_to_sql_server.py
@@ -1,5 +1,10 @@
 # If running in Jupyter Notebook, you'll need to make changes in Jupyter's config file by using the following link:
 # https://stackoverflow.com/questions/43288550/iopub-data-rate-exceeded-in-jupyter-notebook-when-viewing-image
+#
+# I also found that the following installation steps were necessary:
+#
+# 1. Installation of the Connector/Python Module at this link: https://dev.mysql.com/downloads/connector/python/
+# 2. Installation of the package mysql-connector-python-rf
 
 import mysql.connector
 
@@ -11,5 +16,10 @@ conn = mysql.connector.connect(user = 'redacted', password = 'redacted',
 cur = conn.cursor()
 cur.execute("blah blah blah")
 print(cur.fetchall())
-
 conn.close()
+
+# For interacting with Pandas:
+
+print(pd.read_sql("blah blah blah", conn))
+conn.close()
+
